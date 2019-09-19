a9fc4949ca0e1a224a702407dee39f451e37687c
ru <rudolpharthur@gmail.com>
2019-09-19 09:48:12 -0700

    change form size

--- PictureViewer/Form1.Designer.cs
+++ PictureViewer/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(532, 310);
             this.Name = "Form1";
             this.Text = "Imogen Viewer";
             this.ResumeLayout(false);
