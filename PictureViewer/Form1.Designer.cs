34283002f24202c676f27f62aa170f0f3e65ef2f
ru <rudolpharthur@gmail.com>
2019-09-19 02:16:23 -0700

    Revert "changed to view"
    This reverts commit fda361617fe5e64959138b6be874243b219b1677.

--- PictureViewer/Form1.Designer.cs
+++ PictureViewer/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
-            this.Text = "Imogen View";
+            this.Text = "Imogen Viewer";
             this.ResumeLayout(false);
 
         }
