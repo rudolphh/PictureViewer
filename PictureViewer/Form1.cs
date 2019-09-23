abdfcf055be7cf5461602df484faa23ae4c27fca
ru <rudolpharthur@gmail.com>
2019-09-23 07:52:40 -0700

    write code for button handlers

--- PictureViewer/Form1.cs
+++ PictureViewer/Form1.cs
@@ -19,27 +19,43 @@ namespace PictureViewer
 
         private void ShowButton_Click(object sender, EventArgs e)
         {
-
+            // Show the Open File dialog. If the user clicks OK, 
+            // load the picture that the user chose. 
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
         }
 
         private void ClearButton_Click(object sender, EventArgs e)
         {
-
+            // clear the picture
+            pictureBox1.Image = null;
         }
 
         private void BackgroundButton_Click(object sender, EventArgs e)
         {
-
+            // Show the color dialog box. If the user clicks OK, change the
+            // PictureBox control's background to the color the user chose.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
         }
 
         private void CloseButton_Click(object sender, EventArgs e)
         {
-
+            // close the form.
+            this.Close();
         }
 
         private void CheckBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            // If the user selects the Stretch check box,
+            // change the Picturebox's SizeMode property to "Stretch".
+            // If the user clears the check box, change it to "Normal".
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
         }
     }
 }
